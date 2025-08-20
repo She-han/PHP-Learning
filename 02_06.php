79205d6bb3f803f2b1c2845f80de2bf42e3d1e69
Shehan Garusingarachchige <shehangarusinghe@gmail.com>
2025-08-21 01:26:07 +0530

    ternary

---
+++ 02_06.php
@@ -0,0 +1,6 @@
+<?php   
+//explain below by comments
+  $a = 50;   // Assigning value 40 to variable a
+  $a > 45 ? $b=20 : $b=5;   // Ternary operator: if a is greater than 45, b is 20, else b is 5
+  echo "The value of b is $b" ; // Output the value of b
+?>  
